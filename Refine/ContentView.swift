1d24e65e252da92ecb33d38638fd8821e2add512
pakuVision <boardguy.vision@gmail.com>
2026-02-09 19:43:47 +0900

    Initial Commit

---
+++ Refine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Refine
+//
+//  Created by boardguy.vision on 2026/02/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Refine/RefineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefineApp.swift
+//  Refine
+//
+//  Created by boardguy.vision on 2026/02/09.
+//
+
+import SwiftUI
+
+@main
+struct RefineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
